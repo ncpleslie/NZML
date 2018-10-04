ad498f4a64d28bf0fcd7c92ff363463431c11b9a
ncpleslie <nil0310@arastudent.ac.nz>
2018-10-04 20:21:57 +1300

    Added search input

--- nzml.js
+++ nzml.js
@@ -8,31 +8,40 @@ function expandMenu(x) {
             let logo = document.getElementById('logo-horizontal')
             let searchIcon = document.getElementById('search-icon')
             let navBar = document.getElementsByClassName('navbar')
+            let searchBox = document.getElementById('search-box')
 
             if (menuOpen) {
                 menuOpen = false
+
                 links[0].style.display = 'none'
                 logo.style.display = ''
                 searchIcon.style.display = ''
                 navBar[0].style.justifyContent = 'space-between'
             } else {
                 menuOpen = true
+
                 links[0].style.display = 'flex'
                 links[0].style.flexDirection = 'column'
                 logo.style.display = 'none'
                 searchIcon.style.display = 'none'
                 navBar[0].style.justifyContent = 'flex-end'
+                searchBox.style.width = '0%'
             }         
         }
 
 function searchFunction() {
     let logo = document.getElementById('logo-horizontal')
-
+    let searchBox = document.getElementById('search-box')
+    
     if (searchOpen) {
         searchOpen = false
+
         logo.style.display = ''
+        searchBox.style.width = '0%'
     } else {
         searchOpen = true
+        
         logo.style.display = 'none'
+        searchBox.style.width = '80%'
     }
 }
