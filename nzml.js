b0fb27d375f9dee702bf1166bf18fcb3490dd477
ncpleslie <nil0310@arastudent.ac.nz>
2018-10-04 16:43:41 +1300

    Start of day 3

--- nzml.js
+++ nzml.js
@@ -1,14 +1,25 @@
 let open = false
 
 function expandMenu(x) {
-            x.classList.toggle("change");
+            x.classList.toggle("change")
+            
             let links = x.getElementsByClassName("menu-links")
+            let logo = document.getElementById('logo-horizontal')
+            let searchIcon = document.getElementById('search-icon')
+            let navBar = document.getElementsByClassName('navbar')
+
             if (open) {
                 open = false
                 links[0].style.display = 'none'
+                logo.style.display = ''
+                searchIcon.style.display = ''
+                navBar[0].style.justifyContent = 'space-between'
             } else {
                 open = true
                 links[0].style.display = 'flex'
                 links[0].style.flexDirection = 'column'
+                logo.style.display = 'none'
+                searchIcon.style.display = 'none'
+                navBar[0].style.justifyContent = 'flex-end'
             }         
         }
